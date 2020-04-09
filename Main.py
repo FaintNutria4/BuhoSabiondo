da278dc2781bf346c769ef534ff154a0ec8581e6
YerayBartolome <yeray.bartolome@gmail.com>
2020-04-09 20:43:27 +0200

    //WikiReader and its calls on main

--- Main.py
+++ Main.py
@@ -4,12 +4,11 @@ from WikiReader import read
 from TweeterPublisher import publish
 running = True
 
-wikiweb ="https://en.wikipedia.org/wiki/"
-
 while(running):
     theTime = datetime.datetime.now()
-    month = theTime.strftime("%B")
-    day = str(theTime.day)
-    event = read(wikiweb+month+day)
-    publish(event)
+    day = theTime.day
+    month = theTime.month
+    event = read(day, month)
+    print(event)
+    #publish(event)
     time.sleep(86400) # Wait a 24h
--- WikiReader.py
+++ WikiReader.py
@@ -1,4 +1,29 @@
-#import wikipediaapi
+from random import randint
+import wikipediaapi
 
-def read(http):
-    print("do something")
+def read(day, month):
+    wiki_wiki = wikipediaapi.Wikipedia('es')
+    pageName = ("Plantilla:Efemérides - "+str(day)+" de "+parse_month(month))
+    page_py = wiki_wiki.page(pageName)
+    if page_py.exists():
+        print("Wikipedia page ["+pageName+"] succesfully loaded.")
+        return randomEfemeride(page_py)
+    else:
+        print("The Wikipedia page ["+pageName+"] could not be found.")
+        return ("BuhoSabiondo parece estar fallando ——por culpa de Wikipedia——. Meper d0nas¿")
+
+
+def parse_month(month):
+    months = ("enero", "febrero", "marzo", "abril", "mayo", "junio", "julio",
+              "agosto", "septiembre", "octubre", "noviembre", "diciembre")
+    return months[month-1]
+
+
+def randomEfemeride(wikiPage):
+    contents = wikiPage.text
+    contentsList = contents.split('\n')
+    randIndex = randint(0, len(contentsList)-1)
+    #print(contents)
+    #print(randIndex)
+    #print(contentsList[randIndex])
+    return contentsList[randIndex].replace(' (en la imagen)', '')
